601f5d52c89c4c39a520715fe36162070bc3318c
Sarah <sa.alashwal@gmail.com>
2023-09-26 13:25:58 -0400

    create main screen

--- frontend/src/Screens/Notes/Notes.js
+++ frontend/src/Screens/Notes/Notes.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import MainScreen from '../../components/MainScreen';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Notes = () => {
-  return <h1>My Notes</h1>;
+  return (
+    <MainScreen title="Welcom Back Sarah Al-Ashwal">
+      <Link to="createnote">
+        <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+          Create New Note
+        </Button>
+      </Link>
+    </MainScreen>
+  );
 };
 
 export default Notes;
---
+++ frontend/src/data/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+  {
+    _id: '1',
+    title: 'Day 1 of College',
+    content: 'I made few notes and introductions',
+    category: 'collage',
+  },
+  {
+    _id: '2',
+    title: 'Day 2 of College',
+    content: 'Learning new subjects',
+    category: 'collage',
+  },
+  {
+    _id: '3',
+    title: 'work in home',
+    content: 'clean and do shopping',
+    category: 'home',
+  },
+];
+
+module.exports = notes;
